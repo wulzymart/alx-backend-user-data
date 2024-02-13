b00c6504583b9a2f6915efb28a7ff0133588ca13
Olawole Olawale Martins <olawalemart@gmail.com>
2024-02-13 20:50:10 +0100

    Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,7 +6,7 @@ from flask import request, Request
 from typing import List, TypeVar
 
 
-class Auth():
+class Auth:
     """base class for different authenticaton"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
