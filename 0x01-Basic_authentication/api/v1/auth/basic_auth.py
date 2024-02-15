769bcb182c05197e22f1b039f8d20b9aead6eea9
Olawole Olawale Martins <olawalemart@gmail.com>
2024-02-15 18:07:06 +0100

    Basic authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,7 @@
 
 
 from flask import request
-from typing import Tuple, TypeVar
+from typing import TypeVar
 from api.v1.auth.auth import Auth
 from base64 import b64decode
 from models.user import User
@@ -16,13 +16,14 @@ class BasicAuth(Auth):
                                             authorization_header: str)\
             -> str:
         """gets bas64 authorization from header"""
-        if not authorization_header\
-                or not isinstance(authorization_header, str)\
-            or not authorization_header.startswith("Basic "):
+        if not authorization_header or not isinstance(
+            authorization_header, str
+             ) or not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(" ")[1]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
         """decodes base64 encoded str"""
         if not base64_authorization_header or\
                 not isinstance(base64_authorization_header, str):
@@ -33,15 +34,17 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
         """extracts user credentials"""
         cred = decoded_base64_authorization_header
-        if not cred or not isinstance(cred, str) or not ":" in cred:
+        if not cred or not isinstance(cred, str) or ":" not in cred:
             return (None, None)
         email, password = decoded_base64_authorization_header.split(':', 1)
         return (email, password)
 
-    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
         """gets user"""
         if not user_email or not user_pwd or not isinstance(user_pwd, str)\
                 or not isinstance(user_email, str):
